51ed3752b6c259d4894906b20e5e1703a2d965db
David Manzer <dmanzer2@gmail.com>
2025-10-13 09:12:57 -0700

    minor updates

--- js/main.js
+++ js/main.js
@@ -83,25 +83,25 @@ MANZER.slider = function(){
 		slides 					:  	[			// Slideshow Images
 											{
 												image : 'img/slider-images/image01.jpg',
-												title : '<div class="slide-content">DAVID MANZER</div><div class="slide-content-title">Front End Developer / UI/UX Specialist</div>',
+												title : '<div class="slide-content">DAVID MANZER</div><div class="slide-content-title">Sr UI Developer</div>',
 												thumb : '',
 												url : ''
 											},
 											{
 												image : 'img/slider-images/image02.jpg',
-												title : '<div class="slide-content">DAVID MANZER</div><div class="slide-content-title">Front End Developer / UI/UX Specialist</div>',
+												title : '<div class="slide-content">DAVID MANZER</div><div class="slide-content-title">Sr UI Developer</div>',
 												thumb : '',
 												url : ''
 											},
 											{
 												image : 'img/slider-images/image03.jpg',
-												title : '<div class="slide-content">DAVID MANZER</div><div class="slide-content-title">Front End Developer / UI/UX Specialist</div>',
+												title : '<div class="slide-content">DAVID MANZER</div><div class="slide-content-title">Sr UI Developer</div>',
 												thumb : '',
 												url : ''
 											},
 											{
 												image : 'img/slider-images/image04.jpg',
-												title : '<div class="slide-content">DAVID MANZER</div><div class="slide-content-title">Front End Developer / UI/UX Specialist</div>',
+												title : '<div class="slide-content">DAVID MANZER</div><div class="slide-content-title">Sr UI Developer</div>',
 												thumb : '',
 												url : ''
 											}
